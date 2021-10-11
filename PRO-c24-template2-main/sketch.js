309a7d3d145ae3c64afa0013bc0073717de7ca84
EduardoBassani <88408579+EduardoBassani@users.noreply.github.com>
2021-10-11 18:06:50 -0300

    Add files via upload

---
+++ PRO-c24-template2-main/sketch.js
@@ -0,0 +1,35 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var hammer;
+var iron,stone,rubber;
+
+
+
+function setup(){
+    var canvas = createCanvas(1200,600);
+    engine = Engine.create();
+    world = engine.world;
+
+    plane = new Plane(600,height,1200,20);
+    hammer = new Hammer(10,100);
+
+    //iron = new iron(400,200);
+    //stone = new stone(350,200);
+    //rubber = new rubber(125,300,60);
+}
+
+function draw(){
+    background("lightBlue");
+    Engine.update(engine);
+
+
+    plane.display();
+    hammer.display();
+    
+    //iron.display();
+    stone.display();
+    rubber.display();
+}
